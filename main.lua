f8da3f66dbbc9c0db8a8e89938586b67282cfec0
Sam Lee <samclee@gmail.com>
2018-08-07 18:11:26 -0700

    ughgh let's do this

---
+++ main.lua
@@ -0,0 +1,20 @@
+-- love shorthands
+lg = love.graphics
+lw = love.window
+lk = love.keyboard
+
+function love.load()
+    lw.setTitle('Telekinese')
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.keypressed(k)
+
+end
